d9e898b8dbe1f67b5976217479273e331c31f636
yuqing yang <mail@example.com>
2022-08-30 05:35:36 +0000

    update test with tensor; MNK to arg; better parameter setting

--- sparta/specializer/jit/kernels.py
+++ sparta/specializer/jit/kernels.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import Any, Union, Optional
-from ast import literal_eval
 import os
 import jinja2
 
@@ -8,19 +8,36 @@ import numpy as np
 import torch
 
 # import cutex
+import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
 
 @dataclass
 class _Parameter:
+
+    class _ParameterMode(Enum):
+        init_only = 1
+        mutable = 2
+        tunable = 3
+
     name: str
-    value: Any
-    is_tuable: bool
-    search_space: list[Any]
+    value: Any = None
+    description: str = None
+    mode: _ParameterMode = _ParameterMode.init_only 
+    # for tunable parameter only
+    default_space: tuple = None # (type, options), type in ['choice']
 
     def __post_init__(self):
-        if self.search_space is not None:
-            assert self.is_tuable
+        if self.default_space is not None:
+            assert self.mode == _Parameter._ParameterMode.tunable
+            space_type, space_opts = self.default_space
+            if space_type == 'choice':
+                assert isinstance(space_opts, list)
+            else:
+                raise ValueError
+    
+    def is_changable(self):
+        return self.mode != _Parameter._ParameterMode.init_only or self.value is None
 
 
 @dataclass
@@ -69,18 +86,24 @@ class KernelBase:
         self.parameters: dict[_Parameter] = {}
         self.ports: list = []
         self.grid: _GridCfg = None
+        self.search_space: dict = None
 
-    def add_parameter(self, name: str, value: Any = None, is_tunable: bool=False, search_space = None):
-        self.parameters[name] = _Parameter(name, value, is_tunable, search_space)
+    #################### parameters ####################
+    def _add_parameter(self, p: _Parameter):
+        self.parameters[p.name] = p
 
-    def set_parameter(self, name, value):
+    def _set_parameter(self, name, value, force: bool=False):
+        if self.parameters[name].value == value:
+            return
+        assert force or self.parameters[name].is_changable(), f'{name} is not changable' 
         self.parameters[name].value = value
 
     def set_parameters(self, dic: dict):
         for name, value in dic.items():
-            self.parameters[name].value = value
+            self._set_parameter(name, value)
+        self._post_set_parameters()
 
-    def pre_kernel_launch(self):
+    def _post_set_parameters(self):
         pass
 
     def get_parameters(self):
@@ -89,6 +112,13 @@ class KernelBase:
     def get_parameter(self, name):
         return self.parameters[name].value
 
+    #################### tuning related ####################
+    def get_search_space(self):
+        if self.search_space is not None:
+            return self.search_space
+        self.search_space = {k:v.default_space for k,v in self.parameters.items() if v.mode =='tunable'}
+
+    #################### ports (inout) ####################
     def add_port(self, name: str, direction: str, shape: list, dtype:str, layout: Union[str,dict]='dense', formula: str=None):
         self.ports.append(_Tensor(name, direction, shape, dtype, layout, formula))
 
@@ -96,6 +126,9 @@ class KernelBase:
         rst = [p for p in self.ports if p.name==name]
         return rst[0] if rst else None
 
+    #################### process ####################
+
+    #################### misc ####################
     def expr_eval(self, e: _Expr):
         dic = self.get_parameters()
         s = ' '.join([str(dic[t]) if t in dic else t for t in e.tokens])
@@ -123,9 +156,12 @@ class TemplateKernelBase(KernelBase):
         src = self.get_kernel_code(params)
         # kernels = cutex.SourceModule(src, options=['-O3'])
         # self.kernel_func_call = getattr(kernels, self.kernel_func_name)
-        self.kernel_func_call = SourceModule(src, options=['-O3']).get_function(self.kernel_func_name)
-        self.pre_kernel_launch()
-
+        try:
+            self.kernel_func_call = SourceModule(src, options=['-O3']).get_function(self.kernel_func_name)
+        except Exception as err:
+            print(err)
+            print(src)
+            raise RuntimeError
 
 class MatMulKernelBase(TemplateKernelBase):
 
@@ -133,11 +169,11 @@ class MatMulKernelBase(TemplateKernelBase):
         super().__init__()
 
         # add parameters
-        self.add_parameter("GLOBAL_M_VALUE")
-        self.add_parameter("GLOBAL_N_VALUE")
-        self.add_parameter("GLOBAL_K_VALUE")
-        self.add_parameter("BIASED")
-        self.add_parameter("TRANSPOSE")
+        self._add_parameter(_Parameter(name="GLOBAL_M_VALUE"))
+        self._add_parameter(_Parameter(name="GLOBAL_N_VALUE"))
+        self._add_parameter(_Parameter(name="GLOBAL_K_VALUE"))
+        self._add_parameter(_Parameter(name="BIASED"))
+        self._add_parameter(_Parameter(name="TRANSPOSE"))
 
         # add inputs and outputs
         self.add_port('A', 'input', _Expr.from_str(["GLOBAL_M_VALUE", "GLOBAL_K_VALUE"]), 'float', 'dense')
@@ -145,21 +181,26 @@ class MatMulKernelBase(TemplateKernelBase):
         self.add_port('C', 'output', _Expr.from_str(["GLOBAL_M_VALUE", "GLOBAL_N_VALUE"]), 'float', 'dense', 'A @ B')
 
         # add tunable parameters
-        self.add_parameter("BLOCK_SIZE_M_VALUE" , is_tunable=True, search_space=[])
-        self.add_parameter("BLOCK_SIZE_N_VALUE" , is_tunable=True, search_space=[])
-        self.add_parameter("BLOCK_SIZE_K_VALUE" , is_tunable=True, search_space=[])
-        self.add_parameter("THREAD_SIZE_M_VALUE", is_tunable=True, search_space=[])
-        self.add_parameter("THREAD_SIZE_N_VALUE", is_tunable=True, search_space=[])
-        self.add_parameter("THREAD_SIZE_K_VALUE", is_tunable=True, search_space=[])
+        self._add_parameter(_Parameter(name="BLOCK_SIZE_M_VALUE" , mode=_Parameter._ParameterMode.tunable, default_space=('choice', [8, 16, 32, 64, 128, 256])))
+        self._add_parameter(_Parameter(name="BLOCK_SIZE_N_VALUE" , mode=_Parameter._ParameterMode.tunable, default_space=('choice', [8, 16, 32, 64, 128, 256])))
+        self._add_parameter(_Parameter(name="BLOCK_SIZE_K_VALUE" , mode=_Parameter._ParameterMode.tunable, default_space=('choice', [8, 16, 32, 64, 128, 256])))
+        self._add_parameter(_Parameter(name="THREAD_SIZE_M_VALUE", mode=_Parameter._ParameterMode.tunable, default_space=('choice', [2, 4, 8, 16, 32])))
+        self._add_parameter(_Parameter(name="THREAD_SIZE_N_VALUE", mode=_Parameter._ParameterMode.tunable, default_space=('choice', [2, 4, 8, 16, 32])))
+        self._add_parameter(_Parameter(name="THREAD_SIZE_K_VALUE", mode=_Parameter._ParameterMode.tunable, default_space=('choice', [2, 4, 8, 16, 32])))
 
         # kernel launching config
         self.threads_per_block = _Expr.from_str(["BLOCK_SIZE_N_VALUE // THREAD_SIZE_N_VALUE", "BLOCK_SIZE_M_VALUE // THREAD_SIZE_M_VALUE"])
         self.blocks_per_grid = _Expr.from_str(["GLOBAL_N_VALUE // BLOCK_SIZE_N_VALUE", "GLOBAL_M_VALUE // BLOCK_SIZE_M_VALUE"])
 
-    def pre_kernel_launch(self):
+    def _post_set_parameters(self):
         threads_per_block = (self.parameters['BLOCK_SIZE_N_VALUE'].value // self.parameters['THREAD_SIZE_N_VALUE'].value, self.parameters['BLOCK_SIZE_M_VALUE'].value // self.parameters['THREAD_SIZE_M_VALUE'].value, 1)
         blocks_per_grid = (self.parameters['GLOBAL_N_VALUE'].value // self.parameters['BLOCK_SIZE_N_VALUE'].value, self.parameters['GLOBAL_M_VALUE'].value // self.parameters['BLOCK_SIZE_M_VALUE'].value, 1)
         self.grid = _GridCfg(threads_per_block, blocks_per_grid)
+        self.mnk = (
+            np.int32(self.get_parameter('GLOBAL_M_VALUE')), 
+            np.int32(self.get_parameter('GLOBAL_N_VALUE')), 
+            np.int32(self.get_parameter('GLOBAL_K_VALUE')), 
+        )
 
 
 class SparseMatMul(MatMulKernelBase):
@@ -167,8 +208,8 @@ class SparseMatMul(MatMulKernelBase):
     def __init__(self, mode: str, transpose: bool=False, bias: bool=False):
         super().__init__()
 
-        self.set_parameter('TRANSPOSE', transpose)
-        self.set_parameter('BIASED', bias)
+        self._set_parameter('TRANSPOSE', transpose, force=True)
+        self._set_parameter('BIASED', bias, force=True)
 
         self.mode = mode
         if mode == 'dsd':
@@ -179,12 +220,23 @@ class SparseMatMul(MatMulKernelBase):
             else:
                 setattr(self, 'matmul', getattr(self, '__dsd_call__'))
             self.get_port('B').to_sparse()
+        else:
+            raise NotImplementedError
 
     def __dsd_call__(self, A, VAL, PTR, IDX, C, bias=None):
-        self.kernel_func_call(A, VAL, PTR, IDX, C, block=self.grid.threads_per_block, grid=self.grid.blocks_per_grid)
+        self.kernel_func_call(
+            A, VAL, PTR, IDX, C, *self.mnk,
+            block=self.grid.threads_per_block, grid=self.grid.blocks_per_grid)
 
     def __dsd_bias_call__(self, A, VAL, PTR, IDX, C, bias):
-        self.kernel_func_call(A, VAL, PTR, IDX, C, bias, block=self.grid.threads_per_block, grid=self.grid.blocks_per_grid)
+        self.kernel_func_call(
+            A, VAL, PTR, IDX, C, bias, *self.mnk,
+            block=self.grid.threads_per_block, grid=self.grid.blocks_per_grid)
 
     # def __call__(self, A:torch.Tensor, B: dict, C: torch.Tensor, bias=None):
         # self.kernel_func_call(A, B['val'], B['row_ptr'], B['col_idx'], C, block=self.grid.threads_per_block, grid=self.grid.blocks_per_grid)
+
+
+class SparseOpBase:
+    def __init__(self, dense_op: torch.nn.Module, config: dict) -> None:
+        pass
--- sparta/specializer/jit/test_jit_sparse_matmul.py
+++ sparta/specializer/jit/test_jit_sparse_matmul.py
@@ -1,43 +1,108 @@
+from dataclasses import dataclass
+from typing import Optional
 import torch
-from sparta.specializer.jit import kernels as jk
 import numpy as np
 import time
 
 from sparta.common import tesa
 from sparta import specializer
+from sparta.specializer.jit import kernels as jk
 
-import pycuda.autoinit
-import pycuda.driver as drv
-import pycuda.gpuarray as ga
-from pycuda.compiler import SourceModule
+# import pycuda.driver as drv
+# import pycuda.gpuarray as ga
+# from pycuda.compiler import SourceModule
 
-def prepare_data(cfg):
-    A = np.random.uniform(size=(cfg['GLOBAL_M_VALUE'], cfg['GLOBAL_K_VALUE'])).astype(np.float32)
-    B = np.random.uniform(size=(cfg['GLOBAL_N_VALUE'], cfg['GLOBAL_K_VALUE'])).astype(np.float32)
-    B_mask = np.random.uniform(size=(
-        cfg['GLOBAL_N_VALUE'] // cfg['BLOCK_SIZE_N_VALUE'],
-        cfg['GLOBAL_K_VALUE'] // cfg['BLOCK_SIZE_K_VALUE'],
-    )) < 0.2
-    B_tesa = tesa.BCSR(
-        dense=B,
-        mask=B_mask,
-        block_size=(cfg['BLOCK_SIZE_N_VALUE'], cfg['BLOCK_SIZE_K_VALUE'])
-    ).sparse
-    B_val = B_tesa['val']
-    bias = np.random.uniform(size=(cfg['GLOBAL_N_VALUE'], )).astype(np.float32)
+class MatmulDSDTest:
 
-    B_mask_tiled = np.zeros((cfg['GLOBAL_N_VALUE'], cfg['GLOBAL_K_VALUE']))
-    for row_idx in range(B_mask.shape[0]):
-        for col_idx in range(B_mask.shape[1]):
-            row_start = row_idx * cfg['BLOCK_SIZE_N_VALUE']
-            row_end = row_start + cfg['BLOCK_SIZE_N_VALUE']
-            col_start = col_idx * cfg['BLOCK_SIZE_K_VALUE']
-            col_end = col_start + cfg['BLOCK_SIZE_K_VALUE']
-            B_mask_tiled[row_start:row_end, col_start:col_end] = B_mask[row_idx, col_idx]
+    def __init__(self, cfg: dict, sparsity: float, transpose: bool, bias: bool) -> None:
+        self.cfg = dict(cfg)
+        self.transponse = transpose
+        self.bias = bias
+        self.sparsity = sparsity
+        assert sparsity > 0. and sparsity < 1.
 
-    B *= B_mask_tiled
-    C_tgt = A @ B.T 
-    return A, B, B_tesa, B_mask, bias, C_tgt
+        # generate random data
+        self.A = torch.rand((cfg['GLOBAL_M_VALUE'], cfg['GLOBAL_K_VALUE']), dtype=torch.float32).cuda()
+        self.B = torch.rand((cfg['GLOBAL_N_VALUE'], cfg['GLOBAL_K_VALUE']), dtype=torch.float32).cuda()
+        self.B_mask = torch.rand((
+            cfg['GLOBAL_N_VALUE'] // cfg['BLOCK_SIZE_N_VALUE'],
+            cfg['GLOBAL_K_VALUE'] // cfg['BLOCK_SIZE_K_VALUE'],
+        )).cuda() > sparsity
+        self.B_mask_upsample = torch.nn.Upsample(scale_factor=(cfg['BLOCK_SIZE_N_VALUE'], cfg['BLOCK_SIZE_K_VALUE']), mode='nearest')(self.B_mask.float().view(1,1,self.B_mask.shape[0],self.B_mask.shape[1])).int().squeeze()
+        # B_mask_upsample = np.zeros((cfg['GLOBAL_N_VALUE'], cfg['GLOBAL_K_VALUE']))
+        # for row_idx in range(self.B_mask.shape[0]):
+        #     for col_idx in range(self.B_mask.shape[1]):
+        #         row_start = row_idx * cfg['BLOCK_SIZE_N_VALUE']
+        #         row_end = row_start + cfg['BLOCK_SIZE_N_VALUE']
+        #         col_start = col_idx * cfg['BLOCK_SIZE_K_VALUE']
+        #         col_end = col_start + cfg['BLOCK_SIZE_K_VALUE']
+        #         B_mask_upsample[row_start:row_end, col_start:col_end] = self.B_mask[row_idx, col_idx]
+        # B_mask_upsample = torch.from_numpy(B_mask_upsample).int()
+        # assert torch.equal(B_mask_upsample, B_mask_upsample_2)
+        self.B = torch.mul(self.B, self.B_mask_upsample)
+        self.Bias = torch.rand((cfg['GLOBAL_N_VALUE'], ), dtype=torch.float32).cuda() if bias else None
+
+        self.GT = torch.mm(self.A, self.B.T)
+        if self.Bias is not None:
+            self.GT = self.GT + self.Bias
+        self.C = torch.empty(self.GT.shape, dtype=torch.float32).cuda()
+
+        B_tesa = tesa.BCSR(
+            dense=self.B.cpu().numpy(),
+            mask=self.B_mask.cpu().numpy(),
+            block_size=(cfg['BLOCK_SIZE_N_VALUE'], cfg['BLOCK_SIZE_K_VALUE'])
+        ).sparse
+        self.B_val = torch.from_numpy(B_tesa['val']).cuda()
+        self.B_ptr = torch.from_numpy(B_tesa['row_ptr']).cuda()
+        self.B_idx = torch.from_numpy(B_tesa['col_idx']).cuda()
+        
+    def test_matmul_kernel(self, kernel: jk.SparseMatMul, repeat: int =1000):
+        kernel.matmul(self.A, self.B_val, self.B_ptr, self.B_idx, self.C, self.Bias)
+        torch.cuda.synchronize()
+        torch.testing.assert_close(self.C,self.GT)
+
+        for _ in range(10):
+            kernel.matmul(self.A, self.B_val, self.B_ptr, self.B_idx, self.C, self.Bias)
+        torch.cuda.synchronize()
+
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+        for _ in range(repeat):
+            kernel.matmul(self.A, self.B_val, self.B_ptr, self.B_idx, self.C, self.Bias)
+        end.record()
+        torch.cuda.synchronize()
+        t = start.elapsed_time(end) / repeat
+        print(f'PyCuda Latency: {t} ms') 
+        return t       
+
+# def prepare_data(cfg):
+#     A = np.random.uniform(size=(cfg['GLOBAL_M_VALUE'], cfg['GLOBAL_K_VALUE'])).astype(np.float32)
+#     B = np.random.uniform(size=(cfg['GLOBAL_N_VALUE'], cfg['GLOBAL_K_VALUE'])).astype(np.float32)
+#     B_mask = np.random.uniform(size=(
+#         cfg['GLOBAL_N_VALUE'] // cfg['BLOCK_SIZE_N_VALUE'],
+#         cfg['GLOBAL_K_VALUE'] // cfg['BLOCK_SIZE_K_VALUE'],
+#     )) < 0.2
+#     B_tesa = tesa.BCSR(
+#         dense=B,
+#         mask=B_mask,
+#         block_size=(cfg['BLOCK_SIZE_N_VALUE'], cfg['BLOCK_SIZE_K_VALUE'])
+#     ).sparse
+#     B_val = B_tesa['val']
+#     bias = np.random.uniform(size=(cfg['GLOBAL_N_VALUE'], )).astype(np.float32)
+
+#     B_mask_tiled = np.zeros((cfg['GLOBAL_N_VALUE'], cfg['GLOBAL_K_VALUE']))
+#     for row_idx in range(B_mask.shape[0]):
+#         for col_idx in range(B_mask.shape[1]):
+#             row_start = row_idx * cfg['BLOCK_SIZE_N_VALUE']
+#             row_end = row_start + cfg['BLOCK_SIZE_N_VALUE']
+#             col_start = col_idx * cfg['BLOCK_SIZE_K_VALUE']
+#             col_end = col_start + cfg['BLOCK_SIZE_K_VALUE']
+#             B_mask_tiled[row_start:row_end, col_start:col_end] = B_mask[row_idx, col_idx]
+
+#     B *= B_mask_tiled
+#     C_tgt = A @ B.T 
+#     return A, B, B_tesa, B_mask, bias, C_tgt
 
 
 cfg = {
@@ -52,32 +117,19 @@ cfg = {
     'THREAD_SIZE_N_VALUE': 16
 }
 
-A, B, B_tesa, B_mask, bias, C_tgt = prepare_data(cfg)
-TG = torch.from_numpy(C_tgt).cuda()
-AG = torch.from_numpy(A).cuda()
-BG = {k:torch.from_numpy(v).cuda() for k,v in B_tesa.items()}
-VALG = torch.from_numpy(B_tesa['val']).cuda()
-PTRG = torch.from_numpy(B_tesa['row_ptr']).cuda()
-IDXG = torch.from_numpy(B_tesa['col_idx']).cuda()
-CG = torch.empty(C_tgt.shape).cuda()
-
+test = MatmulDSDTest(cfg, 0.8, transpose=True, bias=False)
 s = jk.SparseMatMul('dsd', transpose=True)
 s.set_parameters(cfg)
 s.compile()
+test.test_matmul_kernel(s)
 
-s.matmul(AG, VALG, PTRG, IDXG, CG, bias)
-torch.cuda.synchronize()
-torch.testing.assert_close(CG,TG)
+# A, B, B_tesa, B_mask, bias, C_tgt = prepare_data(cfg)
+# TG = torch.from_numpy(C_tgt).cuda()
+# AG = torch.from_numpy(A).cuda()
+# BG = {k:torch.from_numpy(v).cuda() for k,v in B_tesa.items()}
+# VALG = torch.from_numpy(B_tesa['val']).cuda()
+# PTRG = torch.from_numpy(B_tesa['row_ptr']).cuda()
+# IDXG = torch.from_numpy(B_tesa['col_idx']).cuda()
+# CG = torch.empty(C_tgt.shape).cuda()
 
-for _ in range(10):
-    s.matmul(AG, VALG, PTRG, IDXG, CG, bias)
-
-torch.cuda.synchronize()
-
-
-start = time.time()
-for _ in range(1000):
-    s.matmul(AG, VALG, PTRG, IDXG, CG, bias)
-torch.cuda.synchronize()
-print(f'PyCuda Latency: {(time.time() - start)} ms')
 
