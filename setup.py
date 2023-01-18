620e6dd3ab2683ae923061f7a8e639982a2b60a6
nizhen <nizhen>
2023-01-18 17:24:32 +0900

    update

--- setup.py
+++ setup.py
@@ -4,10 +4,22 @@
 import os
 import torch
 from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 
 rootdir = os.path.dirname(os.path.realpath(__file__))
 
 version = '1.0'
+ext_modules = []
+if torch.cuda.is_available():
+    moe_ext = CUDAExtension(name='sparse_moe', sources=['csrc/moe_sparse_forward.cpp',\
+                                                            'csrc/moe_sparse_forward_kernel.cu'],
+                                extra_compile_args=['-std=c++14', '-O3', "-U__CUDA_NO_HALF_OPERATORS__",
+                                                    "-U__CUDA_NO_HALF_CONVERSIONS__", "-U__CUDA_NO_HALF_CONVERSIONS__"])
+    ext_modules.append(moe_ext)
+    seqlen_dynamic_attention_ext = CUDAExtension(name='seqlen_dynamic_sparse_attention_cpp', sources=['csrc/seqlen_dynamic_sparse_attention_forward.cpp',
+                                                            'csrc/seqlen_dynamic_sparse_attention_forward_kernel.cu'],
+                                extra_compile_args=['-std=c++14', '-O3'])
+    ext_modules.append(seqlen_dynamic_attention_ext)
 
 setup(
     name='SparTA',
@@ -21,6 +33,8 @@ setup(
         'pycuda',  # 'pip install pycuda' works for most cases
         'nni',
     ],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension},
     include_package_data=True,
     package_data={
         'sparta.specializer.kernels': ['templates/*.j2', 'look_up_tables/*.csv'],
