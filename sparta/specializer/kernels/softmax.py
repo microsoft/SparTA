fix sparse softmax & BCSR kernel; add 61 LUTs

sparta/specializer/kernels/softmax.py
@@ -1,17 +1,41 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import io
 from typing import Any, Dict, Tuple
 import importlib.resources as res
 
 import torch
 import jinja2
 import numpy as np
+import pandas as pd
 
 from sparta.tuning import TunableItemCfg
-from sparta.specializer.kernels import templates
+from sparta.specializer.kernels import templates, look_up_tables
 from sparta.specializer.kernels.kernel_base import KernelBase, PortConfig
 from sparta.testing import sparse_softmax_forward_reference, sparse_softmax_backward_reference
+
+
+def _get_sparta_softmax_lut():
+    major, minor = torch.cuda.get_device_capability()
+    try:
+        forward_lut_file = f'softmax.forward.sparta.{major}{minor}.csv'
+        forward_lut_text = res.read_text(look_up_tables, forward_lut_file)
+    except FileNotFoundError:
+        forward_lut_file = f'softmax.backward.sparta.default.csv'
+        forward_lut_text = res.read_text(look_up_tables, forward_lut_file)
+    forward_lut = pd.read_csv(io.StringIO(forward_lut_text))
+    try:
+        backward_lut_file = f'softmax.backward.sparta.{major}{minor}.csv'
+        backward_lut_text = res.read_text(look_up_tables, backward_lut_file)
+    except FileNotFoundError:
+        backward_lut_file = f'softmax.backward.sparta.default.csv'
+        backward_lut_text = res.read_text(look_up_tables, backward_lut_file)
+    backward_lut = pd.read_csv(io.StringIO(backward_lut_text))
+    return forward_lut, backward_lut
+
+
+SPARTA_SOFTMAX_FORWARD_LUT, SPARTA_SOFTMAX_BACKWARD_LUT = _get_sparta_softmax_lut()
 
 
 class SparseSoftmaxKernel(KernelBase):
@@ -161,6 +185,10 @@ class SparTASoftmaxKernel(SparseSoftmaxKernel):
 
     __algo__ = 'sparta'
 
+    def __init__(self, compressed: bool = False, dtype: str = 'float'):
+        super().__init__(compressed, dtype)
+        self._lut: pd.DataFrame = None
+
     def _add_parameters(self):
         super()._add_parameters()
         self._add_parameter(
@@ -175,9 +203,20 @@ class SparTASoftmaxKernel(SparseSoftmaxKernel):
         )
         self._add_parameter(
             'ROW_TILE_VALUE',
-            is_tunable=True,
-            search_space=TunableItemCfg('choice', [1, 2, 4, 8, 16])
         )
+
+    def set_parameters(self, params: Dict[str, Any]):
+        super().set_parameters(params)
+        if 'ROW_TILE_VALUE' in params:
+            return
+        BH, BW = self.get_block_shape()
+        BH_filter = self._lut['BH'] == BH
+        BW_filter = self._lut['BW'] == BW
+        row = self._lut[BH_filter & BW_filter]
+        assert len(row) > 0, f'block shape ({BH}, {BW}) not found in LUT'
+        assert row['latency'] < float('inf'), f'block shape ({BH}, {BW}) is invalid'
+        row = row.reset_index(drop=True).iloc[0, :]
+        self.set_parameter('ROW_TILE_VALUE', int(row['RT']))
 
     def blocks_per_grid(self):
         batch_size, H, W = self.get_shape()
@@ -185,14 +224,18 @@ class SparTASoftmaxKernel(SparseSoftmaxKernel):
         return (H // RT, batch_size, 1)
 
     def threads_per_block(self) -> Tuple[int]:
+        BW = self.get_parameter('BLOCK_SIZE_W_VALUE')
         RT = self.get_parameter('ROW_TILE_VALUE')
-        return (RT * 32, 1, 1)
+        return (RT * min(BW, 32), 1, 1)
 
     def _check_parameters(self, params: Dict[str, Any]):
         BH = params['BLOCK_SIZE_H_VALUE']
         BW = params['BLOCK_SIZE_W_VALUE']
-        RT = params['ROW_TILE_VALUE']
-        assert BH > RT
+        assert BH & (BH - 1) == 0
+        assert BW & (BW - 1) == 0
+        if 'ROW_TILE_VALUE' in params:
+            RT = params['ROW_TILE_VALUE']
+            assert BH >= RT
 
     def get_kernel_code(self):
         template_file = f'{self.__algo__}_sparse_softmax_{self.__direction__}.cuh.j2'
@@ -202,9 +245,13 @@ class SparTASoftmaxKernel(SparseSoftmaxKernel):
 
 class SparTASparseSoftmaxForwardKernel(SparseSoftmaxForwardKernel, SparTASoftmaxKernel):
 
-    pass
+    def __init__(self, compressed: bool = False, dtype: str = 'float'):
+        super().__init__(compressed, dtype)
+        self._lut = SPARTA_SOFTMAX_FORWARD_LUT
 
 
 class SparTASparseSoftmaxBackwardKernel(SparseSoftmaxBackwardKernel, SparTASoftmaxKernel):
 
-    pass
+    def __init__(self, compressed: bool = False, dtype: str = 'float'):
+        super().__init__(compressed, dtype)
+        self._lut = SPARTA_SOFTMAX_BACKWARD_LUT

sparta/tesa/block_compressed.py
@@ -96,11 +96,15 @@ class BCSRIndexes(BCSIndexes):
         block_mask_val = torch.ones(
             size=(self.block_nnz, ),
             dtype=torch.uint8,
-            device=self.row_ptr.device
+            device=self.row_ptr.device,
         )
-        row_ptr = self.row_ptr.to(torch.int64)
-        col_idx = self.BCSR_idx.bitwise_and(0xffff).to(torch.int64)
-        return torch.sparse_csr_tensor(row_ptr, col_idx, block_mask_val).to_dense()
+        col_idx = self.BCSR_idx.bitwise_and(0xffff)
+        return torch.sparse_csr_tensor(
+            crow_indices=self.row_ptr,
+            col_indices=col_idx,
+            values=block_mask_val,
+            size=(self.row_num, self.col_num),
+        ).to_dense()
 
     def convert(self, dense: torch.Tensor):
         return self.function_context.convert(
@@ -144,10 +148,15 @@ class BCSCIndexes(BCSIndexes):
         block_mask_val = torch.ones(
             size=(self.block_nnz, ),
             dtype=torch.uint8,
-            device=self.col_ptr.device
+            device=self.col_ptr.device,
         )
         row_idx = self.BCSC_idx.bitwise_and(0xffff)
-        return torch.sparse_csr_tensor(self.col_ptr, row_idx, block_mask_val).to_dense().T
+        return torch.sparse_csr_tensor(
+            crow_indices=self.col_ptr,
+            col_indices=row_idx,
+            values=block_mask_val,
+            size=(self.col_num, self.row_num),
+        ).to_dense().T
 
     def convert(self, dense: torch.Tensor):
         return self.function_context.convert(

test/lut_maker/sparta_matmul.py
@@ -74,8 +74,7 @@ def test_sparta_matmul_kernel(
     return latency
 
 
-if __name__ == '__main__':
-    _logger.setLevel(logging.DEBUG)
+def make_sparta_matmul_lut():
     major, minor = torch.cuda.get_device_capability()
     lut_file = os.path.join(
         'sparta',
@@ -113,8 +112,13 @@ if __name__ == '__main__':
         _logger.info(f'[{i} / {num}] {params} => {latency} ms')
 
     df = pd.read_csv(log_file)
-    df = df.loc[df.groupby(HYPER_PARAMS).aggregate({'latency': 'idxmin'})]
+    df = df.loc[df.groupby(HYPER_PARAMS).aggregate({'latency': 'idxmin'})['latency']]
     with open(lut_file, 'w') as f:
         f.write(df.reset_index(drop=True).to_csv(index=False))
 
     _logger.info(f'========== Finished. Output: {lut_file} ==========')
+
+
+if __name__ == '__main__':
+    _logger.setLevel(logging.DEBUG)
+    make_sparta_matmul_lut()