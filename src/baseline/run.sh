b253e2fdb07b0f820c8a2af57362c7f6e3151fdd
lei <leiwang1999@outlook.com>
2023-03-27 21:27:54 -0900

    merge

--- src/baseline/run.sh
+++ src/baseline/run.sh
@@ -9,17 +9,10 @@
 # nvcc -forward-unknown-to-host-compiler  -I/usr/local/cuda/include -I${SPUTNIK_ROOT} -I${CUSPARSELT_ROOT}/include -I${SPUTNIK_ROOT}/third_party/abseil-cpp -L/usr/local/cuda/lib64  -L${SPUTNIK_ROOT}/build/sputnik -L${CUSPARSELT_ROOT}/lib64  -lcusparse -lcudart -lcusparseLt -lspmm  --generate-code=arch=compute_80,code=sm_80 -std=c++14  sputnik.cu -o sputnik
 # nvcc -forward-unknown-to-host-compiler  -I/usr/local/cuda/include -I${SPUTNIK_ROOT} -I${CUSPARSELT_ROOT}/include -I${SPUTNIK_ROOT}/third_party/abseil-cpp -L/usr/local/cuda/lib64  -L${SPUTNIK_ROOT}/build/sputnik -L${CUSPARSELT_ROOT}/lib64  -lcusparse -lcudart -lcusparseLt -lspmm  --generate-code=arch=compute_80,code=sm_80 -std=c++14  cusparse_block_ell.cu -o cusparse_block_ell
 
-<<<<<<< HEAD
-# nvcc openai_blocksparse.cu -o openai_blocksparse
-# nvcc -lcublas -o cublas cublas.cu
-# nvcc -gencode arch=compute_80,code=sm_80 -lcusparse -o cusparse cusparse.cu
-#sparsity_ratio=(0.5 0.6 0.7 0.8 0.9)
-=======
 nvcc openai_blocksparse.cu -o openai_blocksparse
 nvcc -lcublas -o cublas cublas.cu
 nvcc -gencode arch=compute_80,code=sm_80 -lcusparse -o cusparse cusparse.cu
 sparsity_ratio=(0.5 0.6 0.7 0.8 0.9)
->>>>>>> adab2c99612f59fc8eadc958114eaf8d2b697625
 #sparsity_ratio=(0.5 0.75 0.9)
 # sparsity_ratio=(0.5)
 #M=(1 16 256 1024 4096)
