10ae8a875df2c5ffae5e3a0a1ef30c88662c7812
nizhen <nizhen>
2023-03-31 18:10:57 +0900

    update end2end script

--- src/Figure12/run.sh
+++ src/Figure12/run.sh
@@ -3,7 +3,20 @@
 conda activate artifact
 align=4
 remain=3
+PWD=`pwd`
+echo $PWD
 pushd /root/nnfusion/Exp_Hardware
 # generate the checkpoint under corrsponding sparsity pattern first 
 python bert_large_balance_ck.py --align $align --remain $remain
 bash run.sh $remain $align
+
+pushd nnfusion_cfg_test/nnfusion_rt/cuda_codegen
+mkdir build
+pushd build
+cmake ..
+make 
+ln -s ../Constant
+./main_test > ${PWD}/end2end_VM_${align}_N_${remain}_M_32.txt
+popd
+popd
+popd
