bb9f5975d460b728a7cb430b1d2954b45b4ac14b
LeiWang1999 <leiwang1999@outlook.com>
2023-04-13 07:00:48 +0000

    update for iteration

--- src/baseline/run.py
+++ src/baseline/run.py
@@ -4,8 +4,8 @@ shape_list=[ (1,1024,1024), (1,2048,2048), (1,4096,4096), (1,8192,8192), (1,1024
 sparsity_ratio=[0.5, 0.75, 0.90625]
 for shape_id in range(1, len(shape_list) + 1):
     m, k, n = shape_list[shape_id]
-    # iterations = 10000 if m == 1 else 100
-    iterations = 1
+    iterations = 10000 if m == 1 else 100
+    # iterations = 1
     for sparsity in sparsity_ratio:    
         print(f"{m} {k} {n} {sparsity}")
         os.system(f'./cublas {sparsity} {n} {k} {m} {iterations} > log/cublas_M{shape_id}_{sparsity:.2f}.log')
