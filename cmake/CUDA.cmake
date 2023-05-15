abbf0233ccf8241ac07b89c653b2b7e7462aa5b5
LeiWang1999 <leiwang1999@outlook.com>
2023-05-15 09:48:57 +0000

    update cuda dependency

--- cmake/CUDA.cmake
+++ cmake/CUDA.cmake
@@ -4,12 +4,6 @@ function(cuda_find_library out_path lib_name)
     PATH_SUFFIXES lib lib64 REQUIRED)
 endfunction()
 
-include(cmake/Cuda.cmake)
-cuda_find_library(CUDART_LIBRARY cudart_static)
-cuda_find_library(CUBLAS_LIBRARY cublas_static)
-cuda_find_library(CUSPARSE_LIBRARY cusparse_static)
-list(APPEND NMSPARSE_LIBS "cudart_static;cublas_static;cusparse_static;culibos;cublasLt_static")
-
 # Helper to create CUDA gencode flags.
 function(create_cuda_gencode_flags out archs_args)
   set(archs ${archs_args} ${ARGN})
