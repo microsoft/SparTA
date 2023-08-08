4f95041459e9296bc0b51cdab500f17b636b50ab
Huiqiang Jiang <hjiang@microsoft.com>
2023-08-08 02:31:53 +0000

    Hotfix(PIT): fix the running scripts

--- script/run_a100.sh
+++ script/run_a100.sh
@@ -1,5 +1,8 @@
-pushd script/figure8 && bash run.sh && popd
-pushd script/figure13 && bash run.sh && popd
+pushd script/figure8 && bash run.sh 
+popd
+
+pushd script/figure13 && bash run.sh 
+popd
 
 mkdir -p results
 cp script/figure*/*.pdf results/
--- script/run_v100.sh
+++ script/run_v100.sh
@@ -1,11 +1,26 @@
-pushd script/figure9 && bash run.sh && popd
-pushd script/figure10 && bash run.sh && popd
-pushd script/figure11 && bash run.sh && popd
-pushd script/figure12 && bash run.sh && popd
-pushd script/figure14 && bash run.sh && popd
-pushd script/figure15 && bash run.sh && popd
-pushd script/figure16 && bash run.sh && popd
-pushd script/figure17 && bash run.sh && popd
+pushd script/figure9 && bash run.sh
+popd
+
+pushd script/figure10 && bash run.sh
+popd
+
+pushd script/figure11 && bash run.sh
+popd
+
+pushd script/figure12 && bash run.sh
+popd
+
+pushd script/figure14 && bash run.sh
+popd
+
+pushd script/figure15 && bash run.sh
+popd
+
+pushd script/figure16 && bash run.sh
+popd
+
+pushd script/figure17 && bash run.sh
+popd
 
 mkdir -p results
 cp script/figure*/*.pdf results/
